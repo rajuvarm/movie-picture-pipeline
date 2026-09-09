d6c5779335fd67e2413e7b0a49c2fc97ba6539b3
Chintham Rajkumar <chinthamrajkumar12@gmail.com>
2026-09-09 20:31:44 +0530

    Fix assertion for movie title in test

--- starter/backend/test_app.py
+++ starter/backend/test_app.py
@@ -14,7 +14,6 @@ def test_movies_endpoint_returns_json():
         response = client.get("/movies/")
         assert response.is_json
 
-
 def test_movies_endpoint_returns_valid_data():
     with app.test_client() as client:
         response = client.get("/movies/")
